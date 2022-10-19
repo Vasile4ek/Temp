cc495ca2fbc838471e215f9b9c260b5b1810ec83
Vasile4ek <110100457+Vasile4ek@users.noreply.github.com>
2022-10-19 20:14:00 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+INPUT_DIR   = 'FROM'
+OUTPUT_DIR  = 'TO'
+MODEL_DIR   = 'models/ru'
+SAMPLE_RATE = 16000
+LOG_LEVEL   = -1 # VOSK: You can set log level to -1 to disable debug messages
+FFMPEG_DIR  = '' # 'C:/../ffmpeg.exe', leave this empty if ffmpeg in PATH
+WAVE_MODE   = False # True - wave only w/out ffmpeg 
+PRINTS_OFF  = False
